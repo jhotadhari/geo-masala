9f3e714af2362b14074b270a496d32279c7705c3
jhotadhari <happyhomelesshippie@allcyclistsarebeautiful.com>
2018-08-20 17:38:58 +0530

    compatible with Gutenberg v3.6.2 and WordPress v4.9.8, and some fixes
    - fix: lazy load script on frontend. only if el .wp-block-geom-map exist;
    - tweak: enqueue frontend asset-entry-scripts on all sites, and lazy load large scripts. before enqueue only if global post has block, so failed on archive pages;

--- src/commonJS/geom_block_map_frontend.js
+++ src/commonJS/geom_block_map_frontend.js
@@ -18,9 +18,12 @@ import getMapPlaceholder from './geom_block_map_frontend/functions/getMapPlaceho
 
 document.addEventListener('DOMContentLoaded', () => {
 	wp.api.loadPromise.done( () => {
+		const $targets = $('.wp-block-geom-map');
+
+		if ( $targets.length === 0 ) return;
 
 		// display placeholder element
-		$('.wp-block-geom-map').each( function(){
+		$targets.each( function(){
 			// get block attributes
 			const atts = getBlocksWithDefaults( JSON.parse( this.getAttribute('data-geom-map') ) );
 			// append placeholder
@@ -30,7 +33,7 @@ document.addEventListener('DOMContentLoaded', () => {
 		// load app and start
 		loadJS( [geomData.pluginDirUrl + '/js/geom_block_map_module_FrontendApp.min.js'] ).then( () => {
 			const App = geomData.modules.FrontendApp;
-			$('.wp-block-geom-map').each( function(){
+			$targets.each( function(){
 				const geomMap = new App({
 					element: this,
 				});
